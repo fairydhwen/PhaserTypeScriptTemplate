915df45e5bab0e1b53e6de97b5f64f770665f030
Fairydhwen <fairydhwen@gmail.com>
2016-05-21 09:11:54 +0200

    Class names now start with lower case letter

---
+++ src/gameStates/mainMenu.ts
@@ -0,0 +1,37 @@
+﻿module $safeprojectname$.Client {
+
+    export class MainMenu extends Phaser.State {
+        
+        background: Phaser.Sprite;
+        logo: Phaser.Sprite;
+
+        create() {
+            this.background = this.add.sprite(0, 0, 'titlepage');
+            this.background.alpha = 0;
+
+            this.logo = this.add.sprite(this.world.centerX, -300, 'logo');
+            this.logo.anchor.setTo(0.5);
+
+            this.add.tween(this.background).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true);
+            this.add.tween(this.logo).to({ y: 220 }, 2000, Phaser.Easing.Elastic.Out, true, 500);
+
+            this.game.debug.text("Click the logo to start the game", 0, this.world.height, "red");
+
+            this.input.onDown.addOnce(this.fadeOut, this);
+        }
+
+        fadeOut() {
+            this.add.audio('click', 1, false).play();
+            
+            this.add.tween(this.background).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+            var tween = this.add.tween(this.logo).to({ y: 800 }, 2000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startGame, this);
+        }
+
+        startGame() {
+            this.game.state.start('Level01', true, false);
+        }
+
+    }
+
+}
